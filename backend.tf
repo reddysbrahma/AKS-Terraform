4efeecd8a81fe82adc32aedae295775257fd1f84
reddysbrahma <reddysbrahma@gmail.com>
2022-09-28 12:46:13 +0530

    Add files via upload

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "activedge-terraform-rg"
+    storage_account_name = "activedgeterraform4472"
+    container_name       = "activstage"
+    key                  = "samplestg"
+  }
+}
+ 
